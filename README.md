a74e2d7ad47a6491ab5ca451a328af2a80b2b9ef
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2023-11-07 17:27:08 -0600

    Update

--- README.md
+++ README.md
@@ -1,24 +1,55 @@
 # Generador de Contraseñas en Python
 
-Este proyecto es un programa realizado en Python que permite generar contraseñas aleatorias y guardarlas en un archivo de texto.
+Este proyecto es un generador de contraseñas aleatorias implementado en Python con una interfaz de usuario amigable. Permite generar contraseñas aleatorias y guardarlas en un archivo de texto.
 
 ## Características
-- `Generación de contraseñas aleatorias` : El programa puede generar contraseñas seguras y aleatorias.
-- `Guardado de contraseñas` : Las contraseñas generadas pueden ser guardadas en un archivo de texto para su posterior uso.
 
-## Herramientas de Desarrollo
-- Python
+- **Generación de contraseñas**: El programa puede generar contraseñas aleatorias.
+- **Guardado de contraseñas**: El programa puede guardar las contraseñas generadas en un archivo de texto para su posterior uso.
+- **Interfaz de usuario**: El programa cuenta con una interfaz de usuario intuitiva para facilitar la interacción.
 
----------------------------------------
+## Estructura del Proyecto
 
-# Password Generator in Python
+El proyecto consta de los siguientes archivos:
 
-This project is a program made in Python that allows you to generate random passwords and save them in a text file.
+- `password_generator.py` : Este es el archivo principal que contiene todo el código del generador de contraseñas.
 
-## Characteristics
-- `Random password generation` : The program can generate secure and random passwords.
-- `Save Password` : Generated passwords can be saved to a text file for later use.
+## Requisitos
 
-## Development tools
-- Python
+Para ejecutar este programa, necesitarás tener instalado Python en tu sistema. Puedes descargarlo desde la página oficial de Python.
 
+## Licencia
+
+Este proyecto está bajo la licencia MIT. Consulta el archivo `LICENSE` para obtener más detalles.
+
+-----------------------------------------
+
+# Python Password Generator
+
+This project is a random password generator implemented in Python with a user-friendly interface. It allows you to generate random passwords and save them in a text file.
+
+## Features
+
+- **Password Generation**: The program can generate random passwords.
+- **Password Saving**: The program can save the generated passwords in a text file for later use.
+- **User Interface**: The program has an intuitive user interface to facilitate interaction.
+
+## Project Structure
+
+The project consists of the following files:
+
+- `password_generator.py`: This is the main file that contains all the code for the password generator.
+
+## Requirements
+
+To run this program, you will need to have Python installed on your system. You can download it from the official Python page.
+
+## License
+
+This project is under the MIT license. See the `LICENSE` file for more details.
+
+-----------------------------------------
+
+<p align="center" >
+<img width="260px" height="340px" style="margin: 10px" src="./password_generator.png"> 
+</p>
--- password_generator.py
+++ password_generator.py
@@ -1,15 +1,12 @@
 #Generador de Contraseñas aleatorias / Random Password Generator
 
-#importando las librerias necesarias / Importing the necessary libraries
+#Importando las librerias necesarias / Importing the necessary libraries
 from tkinter import *
 from tkinter import messagebox
 import random as rm
 import os
 
-global verify2
-verify2 = ""
-
-#Caracteres que se usaran para generar la contraseña / Characters that will be used to generate the password
+#Caracteres para generar la contraseña / Characters to generate the password
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 capital_letter = lower_case.upper()
 symbols = "@#%$"
@@ -22,7 +19,8 @@ def password(num):
     show_password.delete(0, END)
     password = ""  
     
-    #Verifica que el numero de caracteres sea mayor a 12 y muestra una alerta / Verify that the number of characters is greater than 12 and display an alert
+    #Verifica que el numero de caracteres sea mayor a 12 si no lo es muestra una advertencia pero aun asi genera la contraseña 
+    #Verify that the number of characters is greater than 12 if it is not, it shows a warning but still generates the password
     if num < 12:
         messagebox.showinfo("Alvertencia" , "Es Recomendable Poner 12 Caracteres como Minimo para una Contraseña Segura")
     
@@ -32,44 +30,41 @@ def password(num):
         num_ran1 = rm.randint(1,4)
         
         if num_ran1 == 1:
-
-            num_ran2 = rm.randint(0,25)
-            password = password + lower_case[num_ran2]
+            
+            password = password + lower_case[rm.randint(0,25)]
 
         elif num_ran1 == 2:
-
-            num_ran2  = rm.randint(0,25)
-            password = password + capital_letter[num_ran2]
+            
+            password = password + capital_letter[rm.randint(0,25)]
 
         elif num_ran1 == 3:
 
-            num_ran3 = rm.randint(0,3)
-            password= password + symbols[num_ran3]
+            password= password + symbols[rm.randint(0,3)]
 
         elif num_ran1 == 4:
-
-            num_ran4 = rm.randint(0,9)
-            password = password + numbers[num_ran4]
+            
+            password = password + numbers[rm.randint(0,9)]
         
+    
     #Guarda la contraseña en un archivo de texto si el usuario lo desea / Save the password in a text file if the user wishes
-    if disable_save.get():
+    if enable_save.get():
         with open(archive, "a", encoding="UTF-8") as archive_write:
             archive_write.write(f"Contraseña : {password}\n")
 
-    #Muestra la contraseña en la interfaz / Show the password in the interface
+    #Muestra la contraseña en el Entry / Show the password in the Entry
     show_password.insert(0, password)
     
 #Funcion que convierte el numero de caracteres a entero / Function that converts the number of characters to integer
 def convert_integer():
-    
-    verify = entry_characters.get()
-    
+      
     #Verifica que el usuario ingresa un numero / Verify that the user enters a number
-    if verify.isdigit():
+    if entry_characters.get().isdigit():
+        
+        #Convierte el numero de caracteres a entero / Converts the number of characters to integer
         number = int(entry_characters.get())
         password(number)
         
-    elif verify == "":
+    elif entry_characters.get() == "":
         messagebox.showinfo("Alvertencia" , "No Ingreso Ningun Numero")
     else:
         messagebox.showinfo("Alvertencia" , "Solo se pueden Ingresar Numeros")
@@ -80,81 +75,87 @@ def update_width(*args):
     new_length = length + 10
     show_password.config(width=new_length)
 
-#Funcion que desactiva la opcion de guardar las contraseñas en un archivo de texto / Function that disables the option to save passwords in a text file
-def disable():
+#Funcion para el guardado de las contraseñas en un archivo de texto / Function for saving passwords in a text file
+def enable():
     
+    #Alvertencia | Warning
+    messagebox.showinfo("Alvertencia" , "Verifique que el Nombre del Archivo no se repita con otro Archivo de Texto por que se sobreescribira")
+    
+    global archive
+    #Obtiene el directorio actual / Get the current directory
     current_directory = os.getcwd()
     verify = name_txt.get()
     
+    #Verifica que el usuario ingrese un nombre para el archivo de texto / Verify that the user enters a name for the text file
     if verify == "":
         messagebox.showinfo("Alvertencia" , "Ingrese un Nombre para el Archivo de Texto")
-        disable_save.set(0)
-        
-    elif verify == verify2 and disable_save.get():
-        messagebox.showinfo("Alvertencia" ,"Se sobreescribira el Archivo de Texto Borrando las Contraseñas Anteriores")
-        disable_save.set(0)
+        enable_save.set(0)
            
-    elif disable_save.get():
-        global archive
+    elif enable_save.get():
+        
         archive = os.path.join(current_directory, name_txt.get() + ".txt")
         open(archive, "w", encoding="UTF-8")
     
-    verify2 = verify
 
 #Interfaz Grafica / Graphic Interface
 window = Tk()
-disable_save = IntVar()
+enable_save = IntVar()
 sv = StringVar()
 sv.trace("w", update_width)
 
 #nombre de la ventana / Name of the window
 window.title("Password Generator")
+window.configure(background="#3D405B")
 
 #Titulo del programa / Title of the program
-Label_0 = Label(window, text="Programa para Generar Contraselas Aleatorias", font=("Arial", 14))
+Label_0 = Label(window, text="Programa para Generar Contraselas Aleatorias", background="#3D405B", font=("Arial", 16), fg="#81B29A")
 Label_0.grid(row=0, column=0, padx=2, pady=2)
 
-Label_empty= Label(window, text=" ")
+#Espacio en blanco / Blank space
+Label_empty= Label(window, text=" ", background="#3D405B")
 Label_empty.grid(row=1, column=0, padx=2, pady=2)
 
 #Ingresar el numero de caracteres / Enter the number of characters
-Label_1 = Label(window, text="Ingrese el Numero de Caracteres", font=("Arial", 13))
+Label_1 = Label(window, text="Ingrese el Numero de Caracteres", background="#3D405B" , font=("Arial, 14"), fg="#F4F1DE")
 Label_1.grid(row=2, column=0, padx=2, pady=2)
-entry_characters = Entry(window, font=("Arial", 14))
+entry_characters = Entry(window, background="#F4F1DE", bd=4, relief="ridge", font=("Arial, 14"), fg="#3D405B")
 entry_characters.grid(row=3, column=0, padx=1, pady=2) 
 
-Label_empty_1= Label(window, text=" ")
+#Espacio en blanco / Blank space
+Label_empty_1= Label(window, text=" ", background="#3D405B")
 Label_empty_1.grid(row=4, column=0, padx=2, pady=2)
 
 #Titulo para guardar las contraseñas en un archivo de texto / Title to save passwords in a text file
-Label_2 = Label(window, text="Guardar la Contraseña en un Archivo .txt", font=("Arial", 14))
+Label_2 = Label(window, text="Guardar la Contraseña en un Archivo .txt", background="#3D405B", font=("Arial", 14), fg="#F4F1DE")
 Label_2.grid(row=5, column=0, padx=5, pady=5)
 
 #Guardar las contraseñas en un archivo de texto / Save passwords in a text file
-checkbutton_save_passwords = Checkbutton(window, font=("Arial", 10), text="Guardar las Contraseñas",  variable = disable_save, command = disable)
+checkbutton_save_passwords = Checkbutton(window, font=("Arial", 10), fg="#F4F1DE", background="#3D405B", text="Guardar las Contraseñas",  variable = enable_save, command = lambda:enable())
 checkbutton_save_passwords.grid(row=7, column=0, padx=2, pady=2)
 
 #nombre del archivo de texto / name of the text file
-Label_3 = Label(window, text="Nombre del Archivo de Texto", font=("Arial", 13))
+Label_3 = Label(window, text="Nombre del Archivo de Texto", background="#3D405B", font=("Arial", 13), fg="#F4F1DE")
 Label_3.grid(row=8, column=0, padx=5, pady=5)
-name_txt = Entry(window, font=("Arial", 14))
+name_txt = Entry(window,background="#F4F1DE", bd=4, relief="ridge", font=("Arial, 14"), fg="#3D405B")
 name_txt.grid(row=9, column=0, padx=1, pady=2)
 
-Label_empty_2= Label(window, text=" ")
+#Espacio en blanco / Blank space
+Label_empty_2= Label(window, text=" ", background="#3D405B")
 Label_empty_2.grid(row=10, column=0, padx=2, pady=2)
 
 #Boton para generar la contraseña / Button to generate the password
-button_generate_password = Button(window, text="Generar Contraseña", width=19, height=1, font=("Arial", 13), command = lambda:convert_integer())
+button_generate_password = Button(window, text="Generar Contraseña", width=19, height=1, background="#E07A5F", font=("Arial", 13), fg="#F4F1DE", command = lambda:convert_integer())
 button_generate_password.grid(row=11, column=0, padx=2, pady=2)
 
 #Mostrar la contraseña generada / Show the generated password
-Label_4 = Label(window, text="Contraseña Generada", font=("Arial", 14))
+Label_4 = Label(window, text="Contraseña Generada", background="#3D405B", font=("Arial", 14), fg="#F4F1DE")
 Label_4.grid(row=12, column=0, padx=2, pady=2)
-show_password = Entry(window, font=("Arial", 14), textvariable = sv)
+show_password = Entry(window, background="#F4F1DE", font=("Arial", 14), fg="#3D405B", textvariable = sv)
 show_password.config(justify="center")
 show_password.grid(row=13, column=0, padx=1, pady=2)
 
-Label_empty_2= Label(window, text=" ")
+#Espacio en blanco / Blank space
+Label_empty_2= Label(window, text=" ", background="#3D405B")
 Label_empty_2.grid(row=14, column=0, padx=2, pady=2)
 
 #mainloop de la ventana / mainloop of the window
